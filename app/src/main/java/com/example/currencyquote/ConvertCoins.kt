09cbab28a416119e5e228216cbd33ae6a9e5180e
Wpompeo <wag_pompeo@yahoo.com.br>
2024-10-24 17:01:05 -0300

    third commit
    development and modification of files

--- app/src/main/java/com/example/currencyquote/ConvertCoins.kt
+++ app/src/main/java/com/example/currencyquote/ConvertCoins.kt
@@ -1,24 +1,70 @@
 package com.example.currencyquote
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
+import com.example.currencyquote.databinding.ActivityConvertCoinsBinding
 
 class ConvertCoins : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConvertCoinsBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_convert_coins)
+        binding = ActivityConvertCoinsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-       val adapter = ArrayAdapter.createFromResource(
-           this,
+        val adapter = ArrayAdapter.createFromResource(
+            this,
             R.array.country_coins,
             android.R.layout.simple_spinner_item
         )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerOne.adapter = adapter
 
+        var one = 0
+        binding.spinnerOne.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    one = position
+                }
 
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+
+        val adapterTwo = ArrayAdapter.createFromResource(
+            this,
+            R.array.country_coins,
+            android.R.layout.simple_spinner_item
+        )
+        adapterTwo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerTwo.adapter = adapter
+
+        var two = 1
+        binding.spinnerTwo.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    two = position
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
     }
 }
--- app/src/main/java/com/example/currencyquote/MainActivity.kt
+++ app/src/main/java/com/example/currencyquote/MainActivity.kt
@@ -2,18 +2,21 @@ package com.example.currencyquote
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.currencyquote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnStart = findViewById<Button>(R.id.btn_start)
-        btnStart.setOnClickListener {
+        binding.btnStart.setOnClickListener {
             val intent = Intent(this, ConvertCoins::class.java)
             startActivity(intent)
         }
     }
 }
+
